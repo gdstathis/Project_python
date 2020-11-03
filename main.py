1daebe681fdcd141752938b0425e2cae3447ef6a
George Stathis <georgestathis1996@yahoo.gr>
2020-11-03 19:25:40 +0200

    First implementation: get data from localhost:8010/route.json

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify
+from pip._vendor import requests
+
 app = Flask(__name__)
 @app.route('/hello/', methods=['GET', 'POST'])
 def welcome():
     return "Hello World!"
+@app.route('/geodata', methods=['GET'])
+def get_data():
+    data=requests.get('http://localhost:8010/route.json')
+    return  data.json()
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
+
